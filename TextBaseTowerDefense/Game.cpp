fed7ba429ceae7308f6776c5d302b4099b3f12dd
Otto Müller <zocken2196@web.de>
2018-12-29 18:59:49 +0100

    added some more game loup stuff, levels, random enemy generation (prolly wont stay like this, but for testing)

--- TextBaseTowerDefense/Game.cpp
+++ TextBaseTowerDefense/Game.cpp
@@ -2,12 +2,12 @@
 
 void Game::Go()
 {
-	//world.SpawnTower({ 20.0f, 20.0f }, eTowers::AOETowerLvl1, "AOE");
-	//world.SpawnTower({ 21.0f, 20.0f }, eTowers::SniperTowerLvl1, "Sniper");
-	//world.SpawnTower({ 22.0f, 20.0f }, eTowers::ShockTowerLvl1, "Shock");
+	world.SpawnTower({ 20.0f, 20.0f }, eTowers::AOETowerLvl1, "AOE");
+	world.SpawnTower({ 21.0f, 20.0f }, eTowers::SniperTowerLvl1, "Sniper");
+	world.SpawnTower({ 22.0f, 20.0f }, eTowers::ShockTowerLvl1, "Shock");
 
 	//world.SpawnEnemy(eEnemies::Goblin, "Goblin");
-	world.SpawnEnemy(eEnemies::Imp, "Imp1");
+	//world.SpawnEnemy(eEnemies::Imp, "Imp1");
 	//world.SpawnEnemy(eEnemies::Imp, "Imp2");
 	//world.SpawnEnemy(eEnemies::Imp, "Imp3");
 	//world.SpawnEnemy(eEnemies::Imp, "Imp4");
@@ -15,6 +15,25 @@ void Game::Go()
 	//world.SpawnEnemy(eEnemies::Riese, "Riese");
 	
 	std::cout << "\n";
-	while (world.Step() != 0);
-}
+	bool gamenotover = true; //obviously schwachsinn
+	while (gamenotover)
+	{
+		//build phase
+		while (world.Build() != 0);
 
+
+		//wave phase
+
+		//do the enemy spawning
+		rng.seed(level);
+		for (unsigned int i = 0; i <= level; i++)
+		{
+			std::uniform_int_distribution<int> Dist(0,4);
+			world.SpawnEnemy((eEnemies)Dist(rng));
+		}
+
+		//move the world
+		while (world.Step() != 0);
+		level++;
+	}
+}
--- TextBaseTowerDefense/Game.h
+++ TextBaseTowerDefense/Game.h
@@ -2,6 +2,7 @@
 #include "World.h"
 #include "Mesh.h"
 #include <iostream>
+#include <random>
 
 class Game
 {
@@ -16,7 +17,9 @@ public:
 	}
 	void Go();
 private:
+	unsigned int level = 1;
 	const std::vector<Vec2<float>> Waypoints = //Start = bottomleft {x,y}
-	{ { 0.0f,0.0f },{ 0.0f,30.0f } };// , { 100.0f,100.0f }, { 100.0f,50.0f }, { 150.0f,50.0f }, { 150.0f,200.0f }, { 200.0f,200.0f }};
+	{ { 0.0f,0.0f }, { 0.0f,100.0f }, { 100.0f,100.0f }, { 100.0f,50.0f }, { 150.0f,50.0f }, { 150.0f,200.0f }, { 200.0f,200.0f } };
 	World world{ Waypoints };
+	std::mt19937 rng;
 };
